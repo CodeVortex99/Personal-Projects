bceceb322e8f5311804a6ac118c4a4867b575d1f
CodeVortex99 <vstudy909@gmail.com>
2025-07-29 17:50:41 +0100

    Updated all the files.

--- Base fOO The AttAck.py
+++ Base fOO The AttAck.py
@@ -3,10 +3,14 @@ lcode = Code.split()
 lcode.reverse()
 word = ""
 Output = []
-for i in lcode:
-    word = i
-    if word[0].upper() == i[0]:
-        Output.append(word)
+vowels = ["a", "e", "i", "o", "u"]
+if any(item in vowels for item in Code):
+    for i in lcode:
+        word = i
+        if word[0].upper() == i[0]:
+            Output.append(word)
+else:
+    print("Invalid")
 
 space = " "
-print(f"says:"{space.join(Output).lower()})
+print(f"says: {space.join(Output).lower()}")
