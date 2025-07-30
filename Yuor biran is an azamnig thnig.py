0e697454c7ed74d6d0140a1208301e769b8b9978
CodeVortex99 <vstudy909@gmail.com>
2025-07-30 13:29:32 +0100

    Created a program for super anagram checker.

---
+++ Yuor biran is an azamnig thnig.py
@@ -0,0 +1,12 @@
+Words = input("Enter words: ")
+tempL = Words.split()
+Word1 = list(tempL[0])
+Word2 = list(tempL[1])
+
+if all(items in Word1 for items in Word2):
+    if Word1[0] == Word2[0] and Word1[-1] == Word2[-1]:
+        print("Super Anagram!")
+    else:
+        print("Huh?")
+else:
+    print("Huh?")
