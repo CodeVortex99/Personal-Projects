ef403c5b9790d4223af9a13d1e2ac0748c30e773
CodeVortex99 <vstudy909@gmail.com>
2025-07-31 11:07:07 +0100

    Modified a few files,

--- Yuor biran is an azamnig thnig.py
+++ Yuor biran is an azamnig thnig.py
@@ -2,10 +2,12 @@ Words = input("Enter words: ")
 tempL = Words.split()
 Word1 = list(tempL[0])
 Word2 = list(tempL[1])
-
-if all(items in Word1 for items in Word2):
-    if Word1[0] == Word2[0] and Word1[-1] == Word2[-1]:
-        print("Super Anagram!")
+if len(Word1) == len(Word2):
+    if all(items in Word1 for items in Word2):
+        if Word1[0] == Word2[0] and Word1[-1] == Word2[-1]:
+            print("Super Anagram!")
+        else:
+            print("Huh?")
     else:
         print("Huh?")
 else:
--- demo.py
+++ demo.py
@@ -1,2 +1,11 @@
-text = "python"
-print(text[-2:-6:-1])
+# text = "python"
+# print(text[-2:-6:-2])
+
+# try:
+#     stuid = int(input("Enter your ID"))
+# except:
+#     print("Enter Positive digits and only digits")
+
+stude = input("Enter your name: ")
+student = stude.split()
+print(student)
