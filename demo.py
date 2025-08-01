68ff5eeaf74168cbfb80e2c413135481469279f4
CodeVortex99 <vstudy909@gmail.com>
2025-08-01 05:43:38 +0100

    Modified this demo file.

--- demo.py
+++ demo.py
@@ -5,7 +5,3 @@
 #     stuid = int(input("Enter your ID"))
 # except:
 #     print("Enter Positive digits and only digits")
-
-stude = input("Enter your name: ")
-student = stude.split()
-print(student)
