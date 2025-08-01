d140443cf4af55a505cbff53fabde78745263f36
CodeVortex99 <vstudy909@gmail.com>
2025-08-01 05:44:24 +0100

    Added a file for checking out robots in line.

---
+++ Robots in Line.py
@@ -0,0 +1,21 @@
+line = input("Line: ")
+words = line.split()
+
+robot_found = False
+robot_word = None
+
+for i, word in enumerate(words):
+    if word.lower() == 'robot':
+        robot_found = True
+        robot_word = word  
+        break
+
+if robot_found:
+    if robot_word.islower():
+        print("There is a small robot in the line.")
+    elif robot_word.isupper():
+        print("There is a big robot in the line.")
+    else:
+        print("There is a medium sized robot in the line.")
+else:
+    print("No robots here.")
