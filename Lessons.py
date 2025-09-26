f1c99303e1d1e82ff18cccafc4f3a3972ab46cea
CodeVortex99 <vstudy909@gmail.com>
2025-09-26 22:57:05 +0100

    Lessons updated with information on 1D and 2D arrays.

---
+++ Lessons.py
@@ -0,0 +1,21 @@
+#Array:
+# Type of List - Contains items of the same data type.
+
+# 1d Array
+
+text = input('Enter text: ').strip() # Removes white spaces on the front and back.
+text = input('Enter text: ').split() # Converts into a list with words as elements.
+
+text = input('Enter text: ')
+TextL = list(text) #Converts the text into list where each character is the element.
+
+fruits = ['mango', 'apple', 'banana', 'orange']
+# The items of the array can be accessed through their index positions.
+# Python follows 0-index system.
+
+# 2D Array
+
+Objects = [['Mango', 'Banana', 'Apple'], ['Pencil', 'Pen', 'Eraser']] # A 2D array contains of items which are in the form of lists.
+
+#Sub-routine
+
